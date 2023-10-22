f84accb948b8277eb715f1150dccc7f7f8ae7b2f
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-10-22 12:05:17 -0600

    feat: Created entity model class

--- BackEnd/rooms/src/main/java/com/example/rooms/model/Room.java
+++ BackEnd/rooms/src/main/java/com/example/rooms/model/Room.java
@@ -1,4 +1,27 @@
 package com.example.rooms.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "board_room")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRoom;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "size", length = 10, nullable = false)
+    private String size;
+
+    private boolean isReserved;
+
+    @Column(name = "reservation_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date reservationDate;
 }
