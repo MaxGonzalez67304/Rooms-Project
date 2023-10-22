51ac8500437ec9811bddffa7f8aa0a1d1c7f874e
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-10-22 11:54:35 -0600

    feat: Created SpringBoot project

---
+++ BackEnd/rooms/src/main/java/com/example/rooms/RoomsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsApplication.class, args);
+	}
+
+}
