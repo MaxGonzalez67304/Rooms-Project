d52c63d696ce834c04b6b1dab3ee7c68d5453b49
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-10-22 12:09:21 -0600

    feat: Created JPA repository

--- BackEnd/rooms/src/main/java/com/example/rooms/repository/RoomRepository.java
+++ BackEnd/rooms/src/main/java/com/example/rooms/repository/RoomRepository.java
@@ -1,4 +1,14 @@
 package com.example.rooms.repository;
 
-public interface RoomRepository {
+import com.example.rooms.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findByReservedTrue();
+    List<Room> findByReservedFalse();
+    List<Room> findBySize(String size);
 }
