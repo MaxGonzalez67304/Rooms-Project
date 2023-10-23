5be23e53092e5783e8ca7a524922e722c7a2aa22
Jesus Maximiliano Gonzalez Frias <gonzalezfriasmax36@gmail.com>
2023-10-22 20:27:53 -0600

    fix: Fixed automatic relase room task

--- BackEnd/rooms/src/main/java/com/example/rooms/service/RoomService.java
+++ BackEnd/rooms/src/main/java/com/example/rooms/service/RoomService.java
@@ -5,8 +5,11 @@ import com.example.rooms.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +18,8 @@ import java.util.Optional;
 public class RoomService {
     @Autowired
     RoomRepository roomRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(RoomService.class);
 
     public List<Room> getReservedRooms() {
         return roomRepository.findByIsReservedTrue();
@@ -56,20 +61,19 @@ public class RoomService {
 
     @Scheduled(fixedRate = 60000)
     public void autoUpdateRoomReservationStatus() {
+        logger.info("Initializing scheduled task...");
         List<Room> allRooms = roomRepository.findAll();
 
         for (Room room : allRooms) {
-            LocalDateTime startTime = LocalDateTime.parse(room.getReservation_start_time(), DateTimeFormatter.ofPattern("HH:mm"));
-            LocalDateTime endTime = LocalDateTime.parse(room.getReservation_end_time(), DateTimeFormatter.ofPattern("HH:mm"));
+            LocalTime endTime = LocalTime.parse(room.getReservation_end_time(), DateTimeFormatter.ofPattern("HH:mm"));
 
-            if (!room.isReserved() && LocalDateTime.now().isAfter(startTime)) {
-                room.setReserved(true);
-                roomRepository.save(room);
-            } else if (room.isReserved() && LocalDateTime.now().isAfter(endTime)) {
+            if (room.isReserved() && LocalTime.now().isAfter(endTime)) {
                 room.setReserved(false);
                 roomRepository.save(room);
+                logger.info("The room number {} has been released.", room.getIdRoom());
             }
         }
+        logger.info("Task completed.");
     }
 
     public void deleteRoom(Long idRoom) {
